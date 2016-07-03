5c7a5bb8feb1defc78535a89495c46aa4fb593ac
Luciano Matsuo <elnanner2@gmail.com>
2016-07-03 16:16:17 -0300

    listo reporte de ganancias

--- Login_Admin_Mostrar_Ganancias.php
+++ Login_Admin_Mostrar_Ganancias.php
@@ -12,77 +12,40 @@ include("Conectar.php");
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>CouchInn | Reporte Ganancias</title>
-    <link rel ="stylesheet" type ="text/css" href ="Estilos_Login_Admin_Modificar_Tipo.css"/>
-    <link rel="stylesheet" href="Login_Admin_Mostrar_Ganancias.css" media="screen" title="no title" charset="utf-8">
-
-
+    <link rel="stylesheet" href="Estilos_Login_Admin_Mostrar_Ganancias.css" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
     <?php
   		include("Login_Admin_Cabeza.php");
   	?>
   	<div id= "cuerpo">
-  		<?php
+  	  <?php
   			include("Login_Admin_Menu.php");
   		?>
-    </div>
-    <div class="contenido-ganancias">
-        <form class="ingreso-fechas"  method="post">
-            Seleccione Fecha Desde: <input type="date" id='fd' name="fechaDesde" value="" min="2016-01-01">
-            Seleccione Fecha Hasta: <input type="date" id='fh' name="fechaHasta" value="" min="2016-01-01">
-            <br>
-            <input type="submit" name="mostrarGanancias" value="Mostrar Resultados" onclick="return validarFechas()">
+		</div>
+      <div class="contenido-ganancias">
+        <div id= "titulo">
+          <p> Reporte de Ganancias </p>
+          <form action="Login_Admin_Mostrar_Ganancias_Listado.php" class="ingreso-fechas"  method="post">
+              <div id="formulario">
+               Seleccione Fecha Desde: <input type="date" id='fd' name="fechaDesde" value="" min="2016-01-01" REQUIRED>
+              </div>
+              <div id="formulario">
+                Seleccione Fecha Hasta: <input type="date" id='fh' name="fechaHasta" value="" min="2016-01-01" REQUIRED>
+              </div>
+              <br>
+              <div id="boton">
+                <input type="submit" name="mostrarGanancias" value="Mostrar resultados" onclick="return validarFechas();">
+                <input type="button" name="cancelar" value="Cancelar" onClick="location.href='Login_Admin_Index.php'"/>
+              </div>
+          </form>
+        </div>
+      </div>
 
-        </form>
-        <table>
-          <thead>
-            <tr>
-              <th>Fecha Desde</th>
-              <th>Fecha Hasta</th>
-							<th>Fecha Alta Usuario</th>
-							<th>Usuario</th>
-              <th style="text-align:left">Ganancia Obtenida</th><!--la tiro a la izq-->
-            </tr>
-
-          </thead>
-          <tbody>
-            <?php
-              if(isset($_POST['mostrarGanancias'])){
-                $fechaDesde = $_POST['fechaDesde'];
-                $fechaHasta = $_POST['fechaHasta'];
-                $consulta = "SELECT * FROM ganancias WHERE (fecha >='".$fechaDesde."') AND (fecha <= '".$fechaHasta."') GROUP BY fecha";
-                $tuplas = mysql_query($consulta,$link);
-                $numTuplas=mysql_num_rows($tuplas);
-                //echo "fecha ".$fechaDesde;
-                echo "numero de tuplas ".$numTuplas;
-								$gananciaTotal = 0;//si hay tuplas las calcula en el if, sino muestra 0
-                if($numTuplas <> 0){//hay tuplas
-                  //for ($i=0; $i < $numTuplas; $i++) {
-                  while($dato =mysql_fetch_array($tuplas)){
-	                  mostrarUsuario($fechaDesde,$fechaHasta,$dato,$numTuplas);//muestro los datos
-                	}
-									$gananciaTotal = $numTuplas*100;
-                }
-                else{
-                  echo " No Existen datos para as fechas ingresadas";
-                }
-              }
-            ?>
-
-          </tbody>
-        </table>
-				<table>
-					<thead>
-							<th style="text-align:right;padding-right: 8px">Ganancia Total</th>
-					</thead>
-				</table>
-				<table>
-					<thead>
-							<th style="text-align:right;padding-right: 8px"><p><?php echo $gananciaTotal ?></p></th>
-					</thead>
-				</table>
-    </div>
-      <script type="text/javascript">
+      <?php
+        include("Pie.php");
+      ?>
+			<script type="text/javascript">
         function validarFechas(){
           var fechaDesde = document.getElementById('fd').value;//fecha del 1er input
           var fechaHasta = document.getElementById('fh').value;//fecha del 2do nput
@@ -90,38 +53,19 @@ include("Conectar.php");
             alert("Se deben ingresar ambas fechas");
             return false;
           }
-          if(fechaDesde>fechaHasta){
-            alert("La fecha 'desde' no puede ser posterior a la fecha 'hasta'");
-            return false;
-          }
-          //paso la validacion entondes retorno true;
-          alert("Desde: "+fechaDesde+"  "+"Hasta: "+fechaHasta);
-          return true;
+					if(fechaDesde > '2016-01-01' && fechaHasta > '2016-01-01' ){
+	          if(fechaDesde>fechaHasta){
+	            alert("La fecha 'desde' no puede ser posterior a la fecha 'hasta'");
+	            return false;
+	          }
+	          //paso la validacion entondes retorno true;
+	          alert("Desde: "+fechaDesde+"  "+"Hasta: "+fechaHasta);
+          	return true;
+					}
+					else{
+						alert('El año debe ser igual o mayor que 2016');
+					}
         }
 				</script>
-      <?php	include("Pie.php");	?>
   </body>
 </html>
-<?php
-function mostrarUsuario($fechaDesde,$fechaHasta,$dato,$numTuplas){
-	$monto = 100;//este se podria poner de otra manera para una futura modif pero no lo pide
-	$queryNombreUsuario = "SELECT Usuario FROM ucomun WHERE (idUComun ='".$dato['idUsuario']."')";
-	$resultadoUsuario = mysql_query($queryNombreUsuario);
-	$nombreUsuario = mysql_fetch_array($resultadoUsuario);
-	$usuarioPost = $nombreUsuario['Usuario'];
-	$_POST['datoUsuario']=$usuarioPost;// para pasar el dato la re puta madreeeeeee!!!no me saliaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!!!!!!aaaaaaaaaaa!!!
-	echo "<tr>";
-	echo "<td>".$fechaDesde."</td>";
-	echo "<td>".$fechaHasta."</td>";
-	echo "<td>".$dato['fecha']."</td>";
-	usuarioTipoEnlace($usuarioPost);
-	echo "<td style='text-align:left'>".$monto."</td>";
-	echo "</tr>";
-}
-function usuarioTipoEnlace($usuario){
-	echo "<td><form action='DatosUsuario.php' method='post'>
-  <input type='submit' style ='background: transparent;border: none;font-weight:bolder';id='botonUsuario' name='datoUsuario' value='".$usuario."' />
-  </form> </td>";
-}
-
- ?>
---
+++ Login_Admin_Mostrar_Ganancias_Listado.php
@@ -0,0 +1,137 @@
+<?php
+session_start();
+if(!isset($_SESSION['estaLoggeadoUsuarioAdmin']) || isset($_SESSION['estaLoggeadoUsuarioAdmin']) && $_SESSION['estaLoggeadoUsuarioAdmin'] == false){
+	die("Ud. no tiene acceso para visitar esta secci&oacute;n.");
+}
+include("Conectar.php");
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>CouchInn | Reporte Ganancias</title>
+    <link rel="stylesheet" href="Estilos_Login_Admin_Mostrar_Ganancias_Listado.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <?php
+  		include("Login_Admin_Cabeza.php");
+  	?>
+  	<div id= "cuerpo">
+  	  <?php
+  			include("Login_Admin_Menu.php");
+  		?>
+		</div>
+      <div class="contenido-ganancias">
+        <div id= "titulo">
+          <p> Resultados Reporte de Ganancias </p>
+      	</div>
+					<table>
+	          <thead>
+	            <tr>
+	              <th>Fecha Desde</th>
+	              <th>Fecha Hasta</th>
+								<th>Fecha Alta Usuario</th>
+								<th>Usuario</th>
+	              <th style="text-align:left">Ganancia Obtenida</th><!--la tiro a la izq-->
+	            </tr>
+	          </thead>
+					</table>
+
+	          <table>
+							<tbody>
+	            <?php
+	              if(isset($_POST['mostrarGanancias'])){
+	                $fechaDesde = $_POST['fechaDesde'];
+	                $fechaHasta = $_POST['fechaHasta'];
+	                $consulta = "SELECT * FROM ganancias WHERE (fecha >='".$fechaDesde."') AND (fecha <= '".$fechaHasta."') GROUP BY fecha";
+	                $tuplas = mysql_query($consulta,$link);
+	                $numTuplas= mysql_num_rows($tuplas);
+	                //echo "fecha ".$fechaDesde;
+	                //echo "numero de tuplas ".$numTuplas;
+									$gananciaTotal = 0;//si hay tuplas las calcula en el if, sino muestra 0
+	                if($numTuplas <> 0){//hay tuplas
+	                  while($dato =mysql_fetch_array($tuplas)){
+		                  mostrarUsuario($fechaDesde,$fechaHasta,$dato,$numTuplas);//muestro los datos
+	                	}
+										$gananciaTotal = $numTuplas*100;
+	                }
+	                else{
+	                  echo '<div id= "formulario">';
+	                    echo "No se encontraron datos para las fechas ingresadas";
+	                  echo '</div>';
+	                }
+	              }
+	            ?>
+	          </tbody>
+		        </table>
+
+						<div class="separador">
+							<!--solamente es para separar las tablas-->
+						</div>
+
+						<table class="tablaGanancia">
+							<thead>
+									<th style="text-align:right;padding-right: 8px">Ganancia Total</th>
+							</thead>
+							<tbody>
+								<tr>
+									<td style="text-align:right;padding-right: 8px">
+										<?php if (isset($gananciaTotal)) { echo $gananciaTotal; } ?></th>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<div class="separador">
+							<!--solamente es para separar las tablas-->
+						</div>
+						<div id="boton">
+							<input type="button" name="volver" value="Volver" onClick="location.href='Login_Admin_Mostrar_Ganancias.php'"/>
+						</div>
+				</div>
+      <script type="text/javascript">
+        function validarFechas(){
+          var fechaDesde = document.getElementById('fd').value;//fecha del 1er input
+          var fechaHasta = document.getElementById('fh').value;//fecha del 2do nput
+          if((fechaDesde == "")||(fechaHasta == "")){//si alguna o 2 de las fechas no fueron seteadas->alert
+            alert("Se deben ingresar ambas fechas");
+            return false;
+          }
+          if(fechaDesde>fechaHasta){
+            alert("La fecha 'desde' no puede ser posterior a la fecha 'hasta'");
+            return false;
+          }
+          //paso la validacion entondes retorno true;
+          alert("Desde: "+fechaDesde+"  "+"Hasta: "+fechaHasta);
+          return true;
+        }
+				</script>
+      <?php	include("Pie.php");	?>
+  </body>
+</html>
+
+
+<?php
+function mostrarUsuario($fechaDesde,$fechaHasta,$dato,$numTuplas){
+	$monto = 100;//este se podria poner de otra manera para una futura modif pero no lo pide
+	$queryNombreUsuario = "SELECT Usuario FROM ucomun WHERE (idUComun ='".$dato['idUsuario']."')";
+	$resultadoUsuario = mysql_query($queryNombreUsuario);
+	$nombreUsuario = mysql_fetch_array($resultadoUsuario);
+	$usuarioPost = $nombreUsuario['Usuario'];
+	$_POST['datoUsuario']=$usuarioPost;// para pasar el dato la re puta madreeeeeee!!!no me saliaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!!!!!!aaaaaaaaaaa!!!
+	echo "<tr>";
+	echo "<td>".$fechaDesde."</td>";
+	echo "<td>".$fechaHasta."</td>";
+	echo "<td>".$dato['fecha']."</td>";
+	usuarioTipoEnlace($usuarioPost);
+	echo "<td >".$monto."</td>";
+	echo "</tr>";
+}
+function usuarioTipoEnlace($usuario){
+	echo "<td><form action='DatosUsuario.php' method='post'>
+  <input type='submit' style ='background: transparent;border: none;font-weight:bolder';id='botonUsuario' name='datoUsuario' value='".$usuario."' />
+  </form> </td>";
+}
+
+ ?>
